c5110f07f2f37b4e8dccc892ecc46b2487962946
Law-renz <92998407+Law-renz@users.noreply.github.com>
2021-12-10 11:03:10 -0600

    navbar, login & signup form was added to the project

--- front-end/shop-front-end/src/app/app.module.ts
+++ front-end/shop-front-end/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductComponent } from './product/product.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     FooterComponent,
-    ProductComponent
+    ProductComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
