48bccdcfa35f8e703776746800e82bf16737b1da
Mansoor Musleh <92472501+mannyRV@users.noreply.github.com>
2021-12-14 00:54:41 -0500

    Added Services
    added product service, user service and employee service

---
+++ front-end/shop-front-end/src/app/employee.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { BehaviorSubject } from 'rxjs';
+import jwt_decode from "jwt-decode"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  employeeStream: BehaviorSubject<any> = new BehaviorSubject({})
+  employeeName: string | null = null;
+
+  doLogin(credentials: any) {
+    this.httpClient.post("http://localhost:8080/employee/login", credentials)
+      .subscribe({
+        next: (response: any) => {
+          localStorage.setItem("token", response.jwt);
+          const decoded: any = jwt_decode(response.jwt);
+          this.employeeName = decoded.sub;
+          this.employeeStream.next({
+            action: "LOGIN_SUCCESS",
+          })
+        },
+        error: (error => {
+          this.employeeStream.next({
+            action: "LOGIN_FAILED",
+            error
+          })
+        })
+      })
+  }
+
+  decodeToken() {
+    let token = localStorage.getItem("token");
+    if (token) {
+      const decoded: any = jwt_decode(token);
+      this.employeeName = decoded.sub;
+    }
+  }
+  hire(formData: any) {
+    formData.authorities = [
+      'ROLE_EMPLOYEE'
+    ]
+    this.httpClient.post("http://localhost:8080/api/employees", formData)
+      .subscribe({
+        next: (response: any) => {
+          this.employeeStream.next({
+            action: "REGISTER_SUCCESS",
+          })
+        }
+      })
+  }
+
+  // fire(employeeId: number) {
+  //   this.httpClient.delete("http://localhost:8080/api/employees/" + employeeId).subscribe({
+  //     next: e => {
+  //       this.employeeStream = this.
+  //     }
+  //   })
+
+  // }
+
+  doLogout() {
+    localStorage.removeItem('token')
+    this.employeeStream.next({
+      action: "LOGOUT_SUCCESS",
+    })
+  }
+
+  isLoggedIn() {
+    let token = localStorage.getItem("token")
+    if (token)
+      return true;
+    else
+      return false;
+  }
+
+  getToken() {
+    let token = localStorage.getItem("token") || null
+    return token;
+  }
+
+  constructor(private httpClient: HttpClient) { }
+}
---
+++ front-end/shop-front-end/src/app/product.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private products:Array<any> = []
+
+  productsStream = new BehaviorSubject({
+    filter: 'ALL',
+    action: '',
+    products: this.products
+  });
+
+  getProducts(){
+    this.httpClient.get("http://localhost:8080/api/products")
+    .subscribe({
+      next:(response:any)=>{
+        this.products = response;
+        this.productsStream.next({
+          filter:'All',
+          action: 'LOAD_PRODUCTS',
+          products: this.products
+        })
+      }
+    })
+  }
+
+  addProduct(newProduct:any){
+    return new Promise((resolve,reject) => {
+      this.httpClient.post("http://localhost:8080/api/products", {
+        name: newProduct.name,
+        type: newProduct.type
+      }).subscribe({
+        next: e => {
+          this.products.push(newProduct);
+          this.productsStream.next({
+            filter:'ALL',
+            action: 'ADD_PRODUCT',
+            products: this.products
+          })
+          resolve(true)
+        }
+      })
+    })
+  }
+
+  deleteProduct(productId: number) {
+
+    this.httpClient.delete("http://localhost:8080/api/products/" + productId).subscribe({
+      next: e => {
+        this.products = this.products.filter(product => product.id !== productId);
+        this.productsStream.next({
+          filter: 'ALL',
+          action: 'DELETE_PRODUCT',
+          products: this.products
+        })
+      }
+    });
+
+  }
+
+
+  constructor(private httpClient: HttpClient) { }
+}
