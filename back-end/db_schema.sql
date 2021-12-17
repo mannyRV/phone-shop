65b2b6f699bcaac815e0ce3e6c0373e3bdd21e60
Mark Mitchell <mark.mitchell@revature.net>
2021-12-17 16:26:11 -0500

    added example products to the database schema
    to add these to your database, copy-paste the queries from db-schema.sql into your sql workbench

--- back-end/db_schema.sql
+++ back-end/db_schema.sql
@@ -1,3 +1,6 @@
+--Don't use the below schema. It should be up to date but if you don't have the database created, let Hibernate do it for you
+--by changing the auto property in application.properties to create.
+
 CREATE DATABASE shopdb;
 
 USE shopdb;
@@ -54,3 +57,31 @@ customer_id int REFERENCES customers,
 description longtext,
 product_id REFERENCES shopproducts
 );
+
+CREATE TABLE cust_roles(
+customer_id int REFERENCES customers,
+authorities varchar(255)
+);
+
+CREATE TABLE emp_roles(
+employee_id int REFERENCES customers,
+authorities varchar(255)
+);
+
+CREATE TABLE man_roles(
+manager_id int REFERENCES customers,
+authorities varchar(255)
+);
+
+--Don't use the above schema. It should be up to date but if you don't have the database created, let Hibernate do it for you
+--by changing the auto property in application.properties to create.
+
+--EXAMPLE PRODUCTS:
+INSERT INTO shopproducts (name, price, quantity, description, image_path) values ("iPhone 99 Pro ImageLite",
+777.77,14,"Newest model of the beloved iPhone line with only the best in cutting edge phone technology!","../assets/iPhone_99.jpg");
+
+INSERT INTO shopproducts (name, price, quantity, description, image_path) values ("Android Blueberry Deluxe Premium",
+55.55,8,"Newest model of the Android line from Google using optimized software and user-friendly apps","../assets/Android_Blueberry.jpg");
+
+INSERT INTO shopproducts (name, price, quantity, description, image_path) values ("LoveCases Sweet! Candy Fit",
+17.43,52,"Show your phone how much you love it with a sweet LoveCases formfitting case!","../assets/LoveCases.jpg");
